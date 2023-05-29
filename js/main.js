3d58122c39e437f833cb85bb3b8e0836db647a8d
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-05-29 18:29:58 +0200

    Fixed display of book data retrieved from API.

--- js/main.js
+++ js/main.js
@@ -3,16 +3,49 @@ document.querySelector('button').addEventListener('click', getBooks);
 function getBooks(){
 
     let authorName = document.querySelector('input').value;
+    let bookDetails = document.querySelector('#book-details');
+    let bookList = document.getElementById('book-list');
+    let image = 
+
+    
 
     fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:"${authorName}"&key=AIzaSyA8dzdC5M-wByAbu0BmoZh8y_lrVvj3f6Y`)
     .then(res => res.json())    //parse response as JSON
     .then(data => {
+
         for(let i = 0; i < data.items.length; i++){
         console.log(data.items[i]);
-        document.querySelector('h2').innerText = data.items[i].volumeInfo.authors;
-        document.querySelector('h3').innerText = data.items[i].volumeInfo.title;
-        document.querySelector('#pubDate').innerText = data.items[i].volumeInfo.publishedDate;
-        document.querySelector('img').src = data.items[i].volumeInfo.imageLinks.smallThumbnail;
+
+        let entry = document.createElement('li');
+        let coverImg = document.createElement('img');
+        coverImg.src = data.items[i].volumeInfo.imageLinks.smallThumbnail;
+        let authors = data.items[i].volumeInfo.authors;
+        let title = data.items[i].volumeInfo.title;
+        let pubDate = data.items[i].volumeInfo.publishedDate;
+        
+
+        entry.appendChild(document.createTextNode(title));
+        bookList.appendChild(entry);
+ 
+        entry.appendChild(document.createTextNode(pubDate));
+        bookList.appendChild(entry);
+
+        entry.appendChild(document.createTextNode(authors));
+        bookList.appendChild(entry);
+
+        bookList.appendChild(coverImg);
+
+
+
+        // entry.appendChild(document.createTextNode(coverImg));
+        // bookList.appendChild(entry);
+
+        // document.querySelector('h2').innerText = data.items[i].volumeInfo.authors;
+        // document.querySelector('h3').innerText = data.items[i].volumeInfo.title;
+        // document.querySelector('#pubDate').innerText = data.items[i].volumeInfo.publishedDate;
+        // document.querySelector('img').src = data.items[i].volumeInfo.imageLinks.smallThumbnail;
+
+
         } 
     })
     .catch( err => {
