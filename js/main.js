ba07ed6e5ab9bcfcbbfce79882ee6fe301079d9e
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-05-31 14:13:23 +0200

    Added styling for the search bar.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,7 @@ document.querySelector('button').addEventListener('click', getBooks);
 function getBooks(){
 
     let authorName = document.querySelector('input').value;
-    let bookDetails = document.querySelector('#book-details');
     let bookList = document.getElementById('book-list');
-    let image = 
-
-    
 
     fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:"${authorName}"&key=AIzaSyA8dzdC5M-wByAbu0BmoZh8y_lrVvj3f6Y`)
     .then(res => res.json())    //parse response as JSON
@@ -17,11 +13,18 @@ function getBooks(){
         console.log(data.items[i]);
 
         let entry = document.createElement('li');
-        let coverImg = document.createElement('img');
-        coverImg.src = data.items[i].volumeInfo.imageLinks.smallThumbnail;
         let authors = data.items[i].volumeInfo.authors;
         let title = data.items[i].volumeInfo.title;
         let pubDate = data.items[i].volumeInfo.publishedDate;
+
+        let previewLink = document.createElement('a');
+        // previewLink = data.items[i].volumeInfo.previewLink;
+
+        let linkText = document.createTextNode('preview');
+        previewLink.appendChild(linkText);
+
+        let coverImg = document.createElement('img');
+        coverImg.src = data.items[i].volumeInfo.imageLinks.smallThumbnail;
         
 
         entry.appendChild(document.createTextNode(title));
@@ -33,12 +36,12 @@ function getBooks(){
         entry.appendChild(document.createTextNode(authors));
         bookList.appendChild(entry);
 
+        entry.appendChild(document.createTextNode(previewLink));
+        bookList.appendChild(entry);
+
         bookList.appendChild(coverImg);
 
-
-
-        // entry.appendChild(document.createTextNode(coverImg));
-        // bookList.appendChild(entry);
+        
 
         // document.querySelector('h2').innerText = data.items[i].volumeInfo.authors;
         // document.querySelector('h3').innerText = data.items[i].volumeInfo.title;
